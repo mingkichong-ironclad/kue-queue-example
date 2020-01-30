718d25f9812b733bfbacda170823b22b500df5e5
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-01-29 21:25:38 -0800

    handle different job events

--- src/client.ts
+++ src/client.ts
@@ -23,6 +23,32 @@ const data = getMockData(thisFileName);
 const priority = prioritiesKeys[Math.floor(Math.random() * prioritiesKeys.length)];
 console.log("Job with", priority, "priority sent data", data, "\n");
 
-queue.create(jobType, data).priority(priority).save().on('enqueue', function(result) {
+queue.create(jobType, data).priority(priority).save()
+.on('enqueue', function(result) {
+  console.log('Enqueued');
   process.exit(0);
+})
+.on('complete', function(result) {
+  console.log('Completed');
+  process.exit(0);
+})
+.on('failed', function(result) {
+  console.log('failed');
+  process.exit(1);
+})
+.on('failed attempt', function(result) {
+  console.log('failed attempt');
+  process.exit(1);
+})
+.on('start', function(result) {
+  console.log('start');
+})
+.on('remove', function(result) {
+  console.log('remove');
+})
+.on('promotion', function(result) {
+  console.log('promotion');
+})
+.on('progress', function(result) {
+  console.log('progress');
 });
