c3875b9399cb92ecf267393aaa8789076781db61
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-07-24 18:00:02 -0700

    code to remove kue jobs

---
+++ src/remove.ts
@@ -0,0 +1,25 @@
+import * as kue from 'kue';
+import { jobType } from './queue';
+
+async function getJobs (status) {
+  return new Promise<kue.Job[]>((resolve, reject) => {
+    kue.Job.rangeByType(jobType, status, 0, -1, 'asc',(err, jobs) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(jobs);
+      }
+    });
+  });
+}
+
+const states = ['complete', 'failed'];
+
+(async () => {
+  for(const state of states){
+    const jobs = await getJobs(state);
+    jobs.forEach((job)=>{
+      console.log(state, 'removed', job.remove().id);
+    });
+  }
+})();
